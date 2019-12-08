63c938a5891076f61f773c942bacba61654d20a0
Norwid Behrnd <nbehrnd@yahoo.com>
2019-12-08 17:01:16 +0100

    add probe_svg.sh, determine pixel-wise dimension of .svg present
    Prior to attempting a conversion of .svg present into .png, identify
    (provided by ImageMagick) shall state a few characteristics about
    the original files.

---
+++ img/probe_svg.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# name:    probe.svg
+# author:  nbehrnd@yahoo.com
+# date:    2019-12-08 (YYYY-MM-DD)
+
+# At least the majority of the .svg in this project are raster images.
+# Prior to attempt their conversion into .png potentially smaller in
+# their file volume, I would like to know about their original dimension
+# in pixels.  This bash script relays the work to identify, which is part
+# of ImageMagick, to provide for example file type, pixel dimension, color
+# space, and file size.
+#
+# After provision of the executable bit (chmod u+x probe_svg.sh), result
+# may be displayed on the CLI by
+#
+# [sh] ./probe_svg.sh
+#
+# or stored in a permanent record
+#
+# [sh] ./probe_svg.sh > report.txt
+#
+# for further processing.
+
+for FILE in *.svg;
+do
+    identify ${FILE}
+done
+
+# END
